c864ba493ddd4250a98e1852da310fb35f007a2a
Vamseedhar Reddy Palapati <vams1991@gmail.com>
2015-11-10 19:37:03 -0700

    Used MultiPoint class

--- src/main/java/ConvexHullPoints.java
+++ src/main/java/ConvexHullPoints.java
@@ -1,9 +1,8 @@
+
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.SparkConf;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -11,89 +10,75 @@ import com.vividsolutions.jts.geom.CoordinateSequence;
 import com.vividsolutions.jts.geom.CoordinateSequenceFactory;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
+import com.vividsolutions.jts.geom.impl.CoordinateArraySequenceFactory;
 import com.vividsolutions.jts.algorithm.ConvexHull;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Serializable;
 import java.util.*;
 
 public class ConvexHullPoints 
 {
-    public static void getConvexPoints( JavaSparkContext sc )
+    public static void getConvexPoints(JavaSparkContext sc )
     {
     	JavaRDD<String> lines = sc.textFile("/home/vamseedhar/Downloads/Test Case/ConvexHullTestData.csv");
-    	JavaRDD<Coordinate> coordinateList = lines.repartition(1).map(new GetInput());   	
-    	JavaRDD<Coordinate> hullPoints = coordinateList.mapPartitions(new ConvexH());
-    	List<Coordinate> hullPointsList = hullPoints.collect();
-    	JavaRDD<Coordinate> hullPointsRDD = sc.parallelize(hullPointsList, 1);
-    	JavaRDD<String> hullPointsString = hullPointsRDD.repartition(1).map(new Function<Coordinate, String>(){
-
+    	JavaRDD<Coordinate> hullPointsRDD = lines.mapPartitions(new ConvexH());
+    	List<Coordinate> hullPointsList = hullPointsRDD.collect();
+    	Coordinate[] inputArray = new Coordinate[hullPointsList.size()];
+    	int j = 0;
+    	for(Coordinate c: hullPointsList) {
+    		inputArray[j] = c;
+    		System.out.println(c.toString());
+    		j++;
+    	}
+    	GeometryFactory geoFactory1 = new GeometryFactory();
+    	MultiPoint mPoint1 = geoFactory1.createMultiPoint(inputArray);
+    	Geometry geo1 = mPoint1.convexHull();
+    	Coordinate[] convexHullResult = geo1.getCoordinates();
+    	System.out.println(convexHullResult.length);
+    	for(int i = 0; i< convexHullResult.length; i++){
+    		System.out.println("x: "+((Coordinate)convexHullResult[i]).x+"y: "+((Coordinate)convexHullResult[i]).y);
+    	}
+    	
+    	JavaRDD<Coordinate> convexHullResultRDD = sc.parallelize(Arrays.asList(convexHullResult), 1);
+    	JavaRDD<String> convexHullResultString = convexHullResultRDD.repartition(1).map(new Function<Coordinate, String>(){
 			public String call(Coordinate hullPoint) throws Exception {
 				// TODO Auto-generated method stub
 				return hullPoint.x+","+hullPoint.y;
 			}
     		
     	});
-    	
-    	hullPointsString.saveAsTextFile("/home/vamseedhar/Downloads/ConvexHullResultData.csv");
-    			 
+    	convexHullResultString.repartition(1).saveAsTextFile("/home/vamseedhar/Downloads/ConvexHullResultData.csv");
     }
 }
 
-class GlobalConvexH implements Function2<Coordinate[], Coordinate[], Coordinate[]> {
 
-	public Coordinate[] call(Coordinate[] pointArray1, Coordinate[] pointArray2)
-			throws Exception {
-		Coordinate[] inputPoints = new Coordinate[pointArray1.length + pointArray2.length];
-    	ConvexHull convexHull = new ConvexHull(inputPoints, new GeometryFactory());
-    	Geometry convexHullGeometry = convexHull.getConvexHull();
-    	System.out.println("-------"+convexHullGeometry);
-    	Coordinate[] convexResult = convexHullGeometry.getCoordinates();
-		
-		return convexResult;
-	}
-	
-}
-
-class ConvexH implements FlatMapFunction<Iterator<Coordinate>, Coordinate>
+@SuppressWarnings("serial")
+class ConvexH implements FlatMapFunction<Iterator<String>, Coordinate>
 
 {
-	public Iterable<Coordinate> call(Iterator<Coordinate> coordinatesIterator) throws Exception {
+	public Iterable<Coordinate> call(Iterator<String> coordinatesIterator) throws Exception {
 		// TODO Auto-generated method stub
 		List<Coordinate> coorList = new ArrayList<Coordinate>();
 		while(coordinatesIterator.hasNext()){
-			coorList.add(coordinatesIterator.next());
+			String[] temp = coordinatesIterator.next().split(",");
+			coorList.add(new Coordinate(Double.parseDouble(temp[0]), Double.parseDouble(temp[1])));
 		}
     	Coordinate[] coorArray = new Coordinate[coorList.size()];
-    	int i = 0;
+     	int i = 0;
     	for(Coordinate c: coorList){
     		coorArray[i] = c;
     		i++;
     	}
-    	ConvexHull convexHull = new ConvexHull(coorArray, new GeometryFactory());
-    	Geometry convexHullGeometry = convexHull.getConvexHull();
-    	Coordinate[] convexResult = convexHullGeometry.getCoordinates();
+    	
+    	GeometryFactory geoFactory = new GeometryFactory();
+    	MultiPoint mPoint = geoFactory.createMultiPoint(coorArray);
+    	Geometry geo = mPoint.convexHull();
+    	Coordinate[] convexResult = geo.getCoordinates();
     	return Arrays.asList(convexResult);
 	}
     
 }
 
 
-class GetInput implements Function<String, Coordinate> {
-
-	public Coordinate call(String s) throws Exception {
-		// TODO Auto-generated method stub
-		return splitStringtoPoint(s);
-	}
-	
-	private Coordinate splitStringtoPoint(String s) {
-        String[] temp = s.split(",");
-        return new Coordinate(Double.parseDouble(temp[0]), Double.parseDouble(temp[1]));
-        
-    }
-	
-}
